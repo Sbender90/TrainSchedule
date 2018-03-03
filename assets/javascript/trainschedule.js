738ca3157b53c462de19c97497af8d3dec22ceee
Steven Bender <sbender1990@gmail.com>
2018-03-03 00:34:05 -0600

    new

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -1,10 +1,22 @@
-$(this).append(
-    $('<input>', {
-        type: 'text',
-        val: $('#div1').text(),
-        
-    })
-    
-);
+$(function() {
 
-console.log(this);
+var config = {
+    apiKey: "AIzaSyDCpdF9ieNvE4_6NbRNIaTxPJkb0deVxIs",
+    authDomain: "train-scheduler-1beb4.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1beb4.firebaseio.com",
+    projectId: "train-scheduler-1beb4",
+    storageBucket: "",
+    messagingSenderId: "177098785101"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var inputName = "";
+  var inputDestination = "";
+  var inputTime = "";
+  var inputFrequency = "";
+  
+$("#")
+  //
+});
